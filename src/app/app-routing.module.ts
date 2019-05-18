a7a4abf194a9ec7b4b5d210472ca1c8d52e0cc33
Ptit-Biscuit <vincent.brebion@epsi.fr>
2019-05-18 21:37:04 +0200

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {TerminalComponent} from "./terminal/terminal.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: TerminalComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
---
+++ src/app/terminal/terminal.module.ts
@@ -0,0 +1,12 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {TerminalComponent} from './terminal.component';
+
+@NgModule({
+  declarations: [TerminalComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class TerminalModule {
+}
